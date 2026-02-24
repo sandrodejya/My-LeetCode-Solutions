9dda3702eea8afd620ec33d4c1f280b4147c6607
sandrodejya <chrisxu63@gmail.com>
2026-02-24 22:06:49 +0800

    leetcode0224

---
+++ leetcode/editor/cn/[994]腐烂的橘子.java
@@ -0,0 +1,99 @@
+//在给定的 m x n 网格
+// grid 中，每个单元格可以有以下三个值之一：
+//
+//
+// 值 0 代表空单元格；
+// 值 1 代表新鲜橘子；
+// 值 2 代表腐烂的橘子。
+//
+//
+// 每分钟，腐烂的橘子 周围 4 个方向上相邻 的新鲜橘子都会腐烂。
+//
+// 返回 直到单元格中没有新鲜橘子为止所必须经过的最小分钟数。如果不可能，返回 -1 。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//
+//输入：grid = [[2,1,1],[1,1,0],[0,1,1]]
+//输出：4
+//
+//
+// 示例 2：
+//
+//
+//输入：grid = [[2,1,1],[0,1,1],[1,0,1]]
+//输出：-1
+//解释：左下角的橘子（第 2 行， 第 0 列）永远不会腐烂，因为腐烂只会发生在 4 个方向上。
+//
+//
+// 示例 3：
+//
+//
+//输入：grid = [[0,2]]
+//输出：0
+//解释：因为 0 分钟时已经没有新鲜橘子了，所以答案就是 0 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 10
+// grid[i][j] 仅为 0、1 或 2
+//
+//
+// Related Topics 广度优先搜索 数组 矩阵 👍 1127 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int freshCounts = 0;
+        int minutes = 0;
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
+                }
+                if (grid[i][j] == 1) {
+                    freshCounts += 1;
+                }
+            }
+        }
+        while (!queue.isEmpty() && freshCounts > 0) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = queue.poll();
+                int r = curr[0];
+                int c = curr[1];
+                for (int[] direction : directions) {
+                    int newR = r + direction[0];
+                    int newC = c + direction[1];
+                    if ((newR < rows) && (newR >= 0)
+                            && (newC < cols) && (newC >= 0)
+                            && (grid[newR][newC] == 1)) {
+                        grid[newR][newC] = 2;
+                        queue.offer(new int[]{newR, newC});
+                        freshCounts -= 1;
+                    }
+                }
+            }
+            minutes += 1;
+        }
+        if (freshCounts == 0) {
+            return minutes;
+        }
+        return -1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
